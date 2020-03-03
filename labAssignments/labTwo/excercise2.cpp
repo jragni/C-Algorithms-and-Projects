c4a802e7d557d1621ea19b0676dee63e541c2f0b
Jhensen Agni <jagni001@ucr.edu>
2020-03-02 20:58:23 -0800

    completed excercise 2 lab two

---
+++ labAssignments/labTwo/excercise2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char userInput;
+    // user input
+    cout << "Input a character: ";
+    cin >> userInput;
+    printf("The integer equivalent of %c is %d", userInput, userInput);
+
+
+    return 0;
+}
