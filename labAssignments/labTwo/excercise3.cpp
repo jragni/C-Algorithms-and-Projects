234316704ca937449fdc318a99d892ff8cb99041
Jhensen Agni <jagni001@ucr.edu>
2020-03-02 21:10:23 -0800

    changed title of excercise to excercis2.exe

---
+++ labAssignments/labTwo/excercise3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<locale>
+
+using namespace std;
+
+int main(){
+    // get 5 character word from user. Use "proper Capitalization"
+    string userInput;
+
+    if(userInput.size()>5){
+        cout<<"String is too long, try again... Terminating Program";
+        return 0;
+    }
+
+    //
+
+    return 0;
+}
