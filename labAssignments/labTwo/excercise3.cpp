06cfd37bde293dd4fda319737c0eb7f2f639da56
Jhensen Agni <jagni001@ucr.edu>
2020-03-02 21:17:13 -0800

    completed lab 2 excercise 3

--- labAssignments/labTwo/excercise3.cpp
+++ labAssignments/labTwo/excercise3.cpp
@@ -6,13 +6,17 @@ using namespace std;
 int main(){
     // get 5 character word from user. Use "proper Capitalization"
     string userInput;
-
+    cout << "Please input a word with no more than 5 characters: ";
+    cin >> userInput;
     if(userInput.size()>5){
         cout<<"String is too long, try again... Terminating Program";
         return 0;
     }
+    userInput[0] = toupper(userInput[0]);
+    for(int i = 1; i<userInput.size(); i++){
+        userInput[i]=tolower(userInput[i]);
+    }
 
-    //
-
+    cout<<userInput;
     return 0;
 }
