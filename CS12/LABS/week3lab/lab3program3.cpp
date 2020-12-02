6156a6763b3baa1a7bb1849f855476d6fc967b30
Jhensen Ray Agni <jagni001@ucr.edu>
2020-12-01 23:17:48 -0800

    Completed lab3 in CS12

--- CS12/LABS/week3lab/lab3program3.cpp
+++ CS12/LABS/week3lab/lab3program3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "lab3program3.hpp"
 
 // Write a program that allows a user to create a list of cars in their inventory. Create a class "Car" that has the car make, model, type, year, and mileage. Print out the list of cars once the user has completed and write inforation to file
@@ -8,6 +9,7 @@ int main( int argc, char** argv){
 
     // variables
     std::vector<Car> carList;
+    std::ofstream outputFile("vehicleInventory.txt");
     char userInput;
 
 
@@ -19,7 +21,12 @@ int main( int argc, char** argv){
         std::cin >> userInput;
     }while(userInput!='q');
 
-    std::cout << carList.at(0).getMake();
-
+    std::cout << "\nGenerating list...\n"<<"Vehicle Inventory: \n";
+    for(unsigned i = 0; i<carList.size(); i++){
+        std::cout<< i+1 << ": "<< carList.at(i).getYear()<<" "<< carList.at(i).getMake()<<" "<<carList.at(i).getModel()<< " "<<carList.at(i).getMileage()<<"\n";
+        outputFile<< i+1 << ": "<< carList.at(i).getYear()<<" "<< carList.at(i).getMake()<<" "<<carList.at(i).getModel()<< " "<<carList.at(i).getMileage()<<"\n";
+    }
+    std::cout << "Your file has been exported to \"vehicleInventory.cpp\" \n";
+    
     return 1;
 }
